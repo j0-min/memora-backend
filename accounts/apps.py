1139903f29d14da45e2093f7096deea5699d8f6b
JOMIN <jominandjomin@gmail.com>
2026-02-22 22:47:34 +0530

    Temporary auto superuser

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,5 +1,16 @@
 from django.apps import AppConfig
 
-
 class AccountsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    def ready(self):
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
+
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser(
+                username="admin",
+                email="admin@example.com",
+                password="admin123"
+            )
