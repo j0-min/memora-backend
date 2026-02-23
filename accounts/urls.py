7a3749ac44ad481df45ba37f6347f8f9e5bcc6fb
JOMIN <jominandjomin@gmail.com>
2026-02-23 18:49:37 +0530

    Use custom token view for login

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
-from .views import RegisterView
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from .views import RegisterView, CustomTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
-    path('login/', TokenObtainPairView.as_view()),
+    path('login/', CustomTokenObtainPairView.as_view()),
     path('refresh/', TokenRefreshView.as_view()),
 ]
 
