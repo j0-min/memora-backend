e4d1b06a89a0ff000ae23756230e79df02c0b06c
JOMIN <jominandjomin@gmail.com>
2026-02-23 18:23:13 +0530

    Login now returns role fields

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from .models import User
 from .serializers import RegisterSerializer
 
@@ -6,3 +8,16 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
 
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        data["is_patient"] = self.user.is_patient
+        data["is_caregiver"] = self.user.is_caregiver
+
+        return data
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
